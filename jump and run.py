ee1b3510a73747ef226aac480f51dedb7e6152de
funnyMS <holzingermarius@gmail.com>
2025-10-28 14:50:25 +0100

    Initialize pygame and set up game window
    Fixed a typo in the comment and added initial pygame setup.

--- jump and run.py
+++ jump and run.py
@@ -1,2 +1,13 @@
 # jamp-and-run-game-v1.0.0
-We work on a jump and run game
+
+import pygame
+import time
+import random 
+
+pygame.init()
+screen = pygame.display.set_mode([1920,1800])
+pygame.display.set_caption("Jump and run") 
+clock = pygame.time.Clock()
+
+screen.fill(with)
+pygame.draw.rect(screen,(0,255,0),(0, 950, 1800, 50), 0) #blue player
